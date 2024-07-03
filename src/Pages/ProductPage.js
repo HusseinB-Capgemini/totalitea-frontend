2965d4c2d5b9fd9141b1c416b19a9d27075430da
lamajunita <junita.lama@capgemini.com>
2024-07-03 16:12:35 +0100

    product page created

---
+++ src/Pages/ProductPage.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Grid, Card, CardMedia, CardContent, Typography, Button, CircularProgress } from '@mui/material';
+
+const ProductPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8082/api/products');
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <CircularProgress />
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Our Products
+      </Typography>
+      <Grid container spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                component="img"
+                alt={product.name}
+                height="140"
+                image="https://rpack.b-cdn.net/wp-content/uploads/2023/03/custom-coffee-tea-packaging-boxes.jpg"
+              />
+
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+                <Typography variant="h6" component="div">
+                  price:£{product.price}
+                </Typography>
+                 <Typography variant="h6" component="div">
+                     Weight:{product.weight}g
+                     </Typography>
+                <Button variant="contained" color="primary">
+                  Add to Cart
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default ProductPage;
